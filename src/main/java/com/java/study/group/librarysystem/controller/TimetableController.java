c23128ad8ca388bc7ea8e7e5e3157b359996e300
RaquelOliveira <ods.raquel@gmail.com>
2022-09-06 22:40:49 +0100

    LP-6 create courses menu endpoint * Add temporary database h2 * add course model * add course repository

--- src/main/java/com/java/study/group/librarysystem/controller/TimetableController.java
+++ src/main/java/com/java/study/group/librarysystem/controller/TimetableController.java
@@ -1,8 +1,13 @@
 package com.java.study.group.librarysystem.controller;
 
+import com.java.study.group.librarysystem.dto.CourseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("courses")
@@ -11,5 +16,12 @@ public class TimetableController {
   @GetMapping("/timetable")
   public String displayCoursesTimetable() {
     return "timetable";
+  }
+
+  @GetMapping("/timetable-data")
+  public List<CourseDto> listAllCourses(){
+    CourseDto courseDto = new CourseDto("Computers",30, LocalDateTime.now(),"Adult",10L,12,45);
+    return Arrays.asList(courseDto,courseDto,courseDto);
+
   }
 }
