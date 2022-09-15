7963e5acdd09248146c4892ff0743322f7c640a4
RaquelOliveira <ods.raquel@gmail.com>
2022-09-15 19:28:56 +0100

    LP-6 change the return of the method. Add the model.

--- src/main/java/com/java/study/group/librarysystem/controller/TimetableController.java
+++ src/main/java/com/java/study/group/librarysystem/controller/TimetableController.java
@@ -2,6 +2,7 @@ package com.java.study.group.librarysystem.controller;
 
 import com.java.study.group.librarysystem.dto.CourseDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,15 +14,13 @@ import java.util.List;
 @RequestMapping("courses")
 public class TimetableController {
 
+
   @GetMapping("/timetable")
-  public String displayCoursesTimetable() {
-    return "timetable";
-  }
-
-  @GetMapping("/timetable-data")
-  public List<CourseDto> listAllCourses(){
+  public String listAllCourses(Model model){
     CourseDto courseDto = new CourseDto("Computers",30, LocalDateTime.now(),"Adult",10L,12,45);
-    return Arrays.asList(courseDto,courseDto,courseDto);
+    List<CourseDto> courseDtoList =  Arrays.asList(courseDto,courseDto,courseDto);
+    model.addAttribute("coursesDto", courseDtoList);
 
+    return "timetable";
   }
 }
