ce169e8856617117f3a781fe67179b7d1279f654
brunaellen <brunaellen.souza@hotmail.com>
2022-11-30 23:32:33 +0000

    Making some adjustments in the steps to be able to reuse them in the TimeControllerIT.

--- src/test/java/com/java/study/group/librarysystem/ITtest/steps/CommonSteps.java
+++ src/test/java/com/java/study/group/librarysystem/ITtest/steps/CommonSteps.java
@@ -2,31 +2,27 @@ package com.java.study.group.librarysystem.ITtest.steps;
 
 import io.restassured.response.Response;
 import net.thucydides.core.annotations.Step;
-import org.apache.http.HttpStatus;
 
 import static net.serenitybdd.rest.SerenityRest.given;
 import static org.hamcrest.Matchers.is;
 
 public class CommonSteps {
 
-    public Response response;
-
     @Step("Given a GET request to the webapp for the path {0}")
-    public void makeGetRequestToApplication(final String path, String... args) {
-        response = given()
+    public Response makeGetRequestToApplication(final String path, String... args) {
+        return given()
                 .when()
                 .get(path, (Object[]) args);
     }
-    @Step("A {0} response code should be provided")
-    public void verifyResponseStatusCode(int statusCode) {
+    @Step("A {1} response code should be provided")
+    public void verifyResponseStatusCode(final Response response, final int statusCode) {
         response.then()
                 .statusCode(statusCode);
     }
 
     @Step("Verify that the application is UP")
-    public void verifyApplicationIsUp() {
+    public void verifyApplicationIsUp(final Response response) {
         response.then()
-                .statusCode(HttpStatus.SC_OK)
                 .body("status", is("UP"));
     }
 }
