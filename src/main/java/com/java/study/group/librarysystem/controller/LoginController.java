7cd44c39760469acc99c3a90db2cb501e19a5d6c
RaquelOliveira <ods.raquel@gmail.com>
2023-04-01 12:59:04 +0100

    LP-36 * add loginDTO * Fix redirect to login

--- src/main/java/com/java/study/group/librarysystem/controller/LoginController.java
+++ src/main/java/com/java/study/group/librarysystem/controller/LoginController.java
@@ -1,8 +1,11 @@
 package com.java.study.group.librarysystem.controller;
 
+import com.java.study.group.librarysystem.dto.LoginDto;
 import com.java.study.group.librarysystem.model.Login;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,8 +23,8 @@ public class LoginController {
     }
 
     @PostMapping("/signin")
-    public String postLogin(@ModelAttribute Login login, Model model){
-       System.out.println(login.getUsername() + login.getPassword());
+    public String postLogin(LoginDto loginDto){
+       System.out.println(loginDto.getUsername() + loginDto.getPassword());
        return "timetable";
     }
 }
