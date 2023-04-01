d555980efd45aa10d4e68a305691575013644f38
elainegui <elainegui@gmail.com>
2023-04-01 10:51:40 +0100

    LP-36 add thymeleaf to login.html

--- src/main/java/com/java/study/group/librarysystem/controller/LoginController.java
+++ src/main/java/com/java/study/group/librarysystem/controller/LoginController.java
@@ -1,8 +1,11 @@
 package com.java.study.group.librarysystem.controller;
 
+import com.java.study.group.librarysystem.model.Login;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,5 +17,11 @@ public class LoginController {
         model.addAttribute("coursesDto", "coursesDto");
         return "login";
 
+    }
+
+    @PostMapping("/signin")
+    public String postLogin(@ModelAttribute Login login, Model model){
+       System.out.println(login.getUsername() + login.getPassword());
+       return "timetable";
     }
 }
