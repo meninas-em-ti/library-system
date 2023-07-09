037880a081524b01f3e4ecb113c17d1543590086
brunaellen <brunaellen.souza@hotmail.com>
2023-07-09 20:03:39 +0100

    Updating Course controller method to add service layer and adding unit tests.

--- src/main/java/com/java/study/group/librarysystem/controller/CourseController.java
+++ src/main/java/com/java/study/group/librarysystem/controller/CourseController.java
@@ -1,10 +1,10 @@
 package com.java.study.group.librarysystem.controller;
 
 import com.java.study.group.librarysystem.dto.CourseDto;
-import com.java.study.group.librarysystem.model.Course;
-import com.java.study.group.librarysystem.repository.CourseRepository;
+import com.java.study.group.librarysystem.service.CourseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,23 +16,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("course")
 public class CourseController {
 
-    private final CourseRepository repository;
+    private final CourseService courseService;
 
+    // delete this method after frontend changes as will use welcome page.
     @GetMapping("/form/register")
     public String getRegisterForm() {
         return "course";
     }
 
     @PostMapping("/register")
-    public String registerCourse(@ModelAttribute final CourseDto courseDto){
-        System.out.println(courseDto.getName());
-        System.out.println(courseDto.getLimitOfCostumers());
-        System.out.println(courseDto.getPriceOfClass());
-        System.out.println(courseDto.getAgeGroup());
-        System.out.println(courseDto.getInstructorName());
+    public String registerCourse(@ModelAttribute final CourseDto courseDto, final Model model) throws Exception {
+        try {
+            courseService.register(courseDto);
+            return "redirect:/courses";
+        } catch (Exception exception) {
+            model.addAttribute("error", exception.getMessage() + " Error to register course");
+        }
 
-        Course courseSaved = repository.save(courseDto.toCourse());
-
-        return "redirect:/courses";
+        return "course";
     }
 }
