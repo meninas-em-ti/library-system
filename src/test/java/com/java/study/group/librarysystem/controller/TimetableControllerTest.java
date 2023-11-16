63e66e927e9d07544ccad7cd142df9152d4f7c80
brunaellen <brunaellen.souza@hotmail.com>
2023-11-16 22:45:32 +0000

    Updating test to reflect changes made to the logic in the past. [Not related to the current task] but as test was failing, it was necessary to address the issue. * Issues: - there were 2 fields in CourseDto removed - CourseRepository class was added as a field in timetableController.

--- src/test/java/com/java/study/group/librarysystem/controller/TimetableControllerTest.java
+++ src/test/java/com/java/study/group/librarysystem/controller/TimetableControllerTest.java
@@ -1,45 +1,51 @@
 package com.java.study.group.librarysystem.controller;
 
 import com.java.study.group.librarysystem.dto.CourseDto;
+import com.java.study.group.librarysystem.model.Course;
+import com.java.study.group.librarysystem.repository.CourseRepository;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.ui.Model;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TimetableControllerTest {
     @Mock
     private Model modelMock;
 
-    private TimetableController timetableController;
+    @Mock
+    private CourseRepository repository;
 
-    @BeforeEach
-    void setUp() {
-        timetableController = new TimetableController();
-    }
+    @InjectMocks
+    private TimetableController timetableController;
 
     @AfterEach
     void afterEach() {
-        verifyNoMoreInteractions(modelMock);
+        verifyNoMoreInteractions(modelMock, repository);
     }
 
     @Test
     void listAllCourses_returns_timetable_page() {
+        final Course course = new Course(1l, "Computers",30,
+            LocalDateTime.parse("2022-09-26T00:12:43.482047"),"Adult",10L, "Bruna");
+
         final CourseDto expectedCourseDto = new CourseDto("Computers",30,
-                LocalDateTime.parse("2022-09-26T00:12:43.482047"),"Adult",10L,
-                12,45);
+            LocalDateTime.parse("2022-09-26T00:12:43.482047"),"Adult",10L, "Bruna");
+
+        when(repository.findAll()).thenReturn(List.of(course));
 
         assertThat(timetableController.listAllCourses(modelMock)).isEqualTo("timetable");
-        verify(modelMock).addAttribute("coursesDto", Arrays.asList(expectedCourseDto,expectedCourseDto,expectedCourseDto));
+        verify(repository).findAll();
+        verify(modelMock).addAttribute("coursesDto", Arrays.asList(expectedCourseDto));
     }
 }
