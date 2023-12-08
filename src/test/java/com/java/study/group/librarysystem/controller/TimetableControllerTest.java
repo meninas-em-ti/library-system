27b01c65b2b6c2800b2b3e051b5b4f59f12094a4
brunaellen <brunaellen.souza@hotmail.com>
2023-12-08 18:57:51 +0000

    Updating test to reflect changes made in date and time

--- src/test/java/com/java/study/group/librarysystem/controller/TimetableControllerTest.java
+++ src/test/java/com/java/study/group/librarysystem/controller/TimetableControllerTest.java
@@ -1,7 +1,6 @@
 package com.java.study.group.librarysystem.controller;
 
 import com.java.study.group.librarysystem.dto.CourseDto;
-import com.java.study.group.librarysystem.model.Course;
 import com.java.study.group.librarysystem.repository.CourseRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -36,16 +35,13 @@ class TimetableControllerTest {
 
     @Test
     void listAllCourses_returns_timetable_page() {
-        /*final Course course = new Course(1l, "Computers",30,
-            LocalDateTime.parse("2022-09-26T00:12:43.482047"),"Adult",10L, "Bruna");
-
         final CourseDto expectedCourseDto = new CourseDto("Computers",30,
             LocalDateTime.parse("2022-09-26T00:12:43.482047"),"Adult",10L, "Bruna");
 
-        when(repository.findAll()).thenReturn(List.of(course));
+        when(repository.findAll()).thenReturn(List.of(expectedCourseDto.toCourse()));
 
         assertThat(timetableController.listAllCourses(modelMock)).isEqualTo("timetable");
         verify(repository).findAll();
-        verify(modelMock).addAttribute("coursesDto", Arrays.asList(expectedCourseDto));*/
+        verify(modelMock).addAttribute("coursesDto", Arrays.asList(expectedCourseDto));
     }
 }
