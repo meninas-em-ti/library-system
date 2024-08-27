6b97fcc6d9f8848f8d711530cd0b40f1eb51c09b
Ialcantara09 <ivone_alcantara@yahoo.com.br>
2024-08-27 21:09:46 +0100

    Fixed limit_of_customers from timetable

--- src/main/java/com/java/study/group/librarysystem/model/Course.java
+++ src/main/java/com/java/study/group/librarysystem/model/Course.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.checkerframework.checker.units.qual.C;
 
 
 import java.math.BigDecimal;
@@ -20,6 +19,7 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long courseID;
     private String name;
+    @Column(name = "limit_of_customers")
     private int limitOfCostumers;
     @Column(name = "start_date_and_time")
     private LocalDateTime startDateAndTime;
