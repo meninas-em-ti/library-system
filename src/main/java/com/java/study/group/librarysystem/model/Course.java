ab939c487af61ca29440e16320fa22198a53e402
Ialcantara09 <ivone_alcantara@yahoo.com.br>
2024-09-05 21:08:02 +0100

    Drop table course because was duplicated; Rename table Course to course; Add column annotations.

--- src/main/java/com/java/study/group/librarysystem/model/Course.java
+++ src/main/java/com/java/study/group/librarysystem/model/Course.java
@@ -12,18 +12,21 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
+@Entity(name = "course")
 public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long courseID;
     private String name;
     @Column(name = "limit_of_customers")
     private int limitOfCustomers;
     @Column(name = "start_date_and_time")
     private LocalDateTime startDateAndTime;
+    @Column(name = "age_group")
     private String ageGroup;
+    @Column(name = "price_of_class")
     private BigDecimal priceOfClass;
     @Column(name = "instructor_name")
     private String instructorName;
